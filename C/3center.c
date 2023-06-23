d41719558a59011991354edc00e2c55fa36171cd
jasonrwilson <106190831+jasonrwilson@users.noreply.github.com>
2023-06-23 10:17:56 -0400

    Update 3center.c

--- C/3center.c
+++ C/3center.c
@@ -6,7 +6,6 @@
 #define MAX_POINTS 1024
 
 int read_file (float* points, char* filename) {
-
     int n = 0;
     float next[2];
     FILE* file_ptr;
@@ -56,30 +55,12 @@ float center_cost_sq (float* p, int n, int c1, int c2, int c3) {
     return cost_sq;
 }
 
-int main (int argc, char** argv) {
-
-    /* storage for the dataset */
-    float p[2*MAX_POINTS];
-
-    /* read filename from command line */
-    if (argc < 2) {
-	    printf ("Command usage : %s %s\n",argv[0],"filename");
-	    return 1;
-    }
-
-    /* read dataset */
-    int n = read_file (p,argv[1]);
-
-    double min_cost_sq = DBL_MAX;
-    int c[3];
-    int triples_checked = 0;
-
-    /* solve the 3-center problem exactly */
+float solve_3center(float* p, int n, int* c) {
+	float min_cost_sq = DBL_MAX;
     for (int i=0;i<n-2;i++) {
         for (int j=i+1;j<n-1;j++) {
             for (int k=j+1;k<n;k++) {
-		        triples_checked += 1;
-		        double cost_sq = center_cost_sq (p, n, i, j, k);
+		        float cost_sq = center_cost_sq (p, n, i, j, k);
 		        if (cost_sq < min_cost_sq) {
 			        min_cost_sq = cost_sq;
 			        c[0] = i;
@@ -89,9 +70,20 @@ int main (int argc, char** argv) {
 		    }
 	    }
     }
+    return sqrt(min_cost_sq);
+}
 
-    /* print out the number of triples checked */
-    printf ("number of triples checked = %d\n",triples_checked);
+int main (int argc, char** argv) {
+
+    /* storage for the 2d points */
+    float p[2*MAX_POINTS];
+
+    /* read the 2d points */
+    int n = read_file (p,argv[1]);
+
+    /* solve the 3-center problem exactly */
+    int c[3];
+	float min_cost = solve_3center(p,n,c);
 
     /* print the minimal cost for the 3-center problem */
     printf ("minimal cost = %g\n",sqrt(min_cost_sq));
@@ -99,5 +91,6 @@ int main (int argc, char** argv) {
     /* print an optimal solution to the 3 center problem */
     printf ("centers : %d %d %d\n",c[0],c[1],c[2]);
 
+	/* program executed successfully */
     return 0;
 }
